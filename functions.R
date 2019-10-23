06874f22058606c5887c12980245f653b7320902
Ismael Utitiaj <eutitiaj@gmail.com>
2019-10-23 15:07:36 -0500

    primera version subida ok en cloud Shiny.io

---
+++ functions.R
@@ -0,0 +1,370 @@
+library(readr)
+library(twitteR)
+library(dplyr)
+library(tidyr)
+#library(qdap)
+library(sentimentr)
+
+
+
+#funciones basicas 
+
+#proceso #2: estructurar datos, se convierte a DF los tweets obtenidos
+createData <- function(tweets){
+  
+  #Convirtiendo los tweets en un data frame
+  # if(is.list(tweets))
+  #    {
+  #      tweetsDF.df <- twListToDF(tweets)
+  # }
+  # else
+  # {
+  #  tweetsDF.df <- tweets
+  #}
+  tweetsDF.df <- tweets
+  
+  #tweetsDF.df  %>%
+  #  mutate(rowNumber = row_number())
+  
+  text <- tweetsDF.df$text
+  createdDate <- tweetsDF.df$created
+  userName <- tweetsDF.df$screenName
+  #recordNum <- tweetsDF.df$rowNumber
+  datosTweets <- data.frame(text, createdDate, userName) 
+  
+  tweetsFecha <- datosTweets %>%
+    mutate(fechaTweet = as.Date(datosTweets$createdDate)) %>%
+    arrange(fechaTweet)
+  
+  #usersTwitter <- lookupUsers(datosTweets$userName) #retorna screen name, nombre, ubicacion
+  nombre <- tweetsDF.df$screenName
+  usersTwitter <- lookupUsers(nombre) #retorna screen name, nombre, ubicacion
+ 
+  userTwitterDF <- twListToDF(usersTwitter) %>%
+    select(c(screenName, name,location)) %>%
+    separate(name, sep = " ", into = c("Nombre", "Apellido")) %>%
+    separate(location, sep = ",", into = c("Ciudad", "Pais"))
+  
+ 
+  #aplicar funcion para genero de acuerdo al nombre del usuario
+ 
+ 
+  userGenero <- userTwitterDF %>%
+    mutate(Genero =  sapply(userTwitterDF$Nombre, getGender))
+             
+  
+  tweetsUnion <- tweetsFecha %>%
+    left_join( userGenero, by = c("userName" = "screenName"))
+
+#  tweetsUnion %>%
+ #   arrange(recordNum) 
+  
+  #colnames(tweetsUnion) <- c("Texto", "Fecha creación", "Nombre usuario", "Fecha tweet", "Nombre", "Apellido", "Ciudad", "País", "Género")
+  
+  #TODO: pendiente cambiar valor male to hombre
+  #library(plyr)
+  
+ # tweetsUnion$Genero <- revalue(tweetsUnion$Genero, c("male" = "hombre"))
+  #tweetsUnion$Genero <- as.character(tweetsUnion$Genero)
+  #tweetsUnion[tweetsUnion$Genero == "female"] <- "mujer"
+  
+  # tweetsUnion<- tweetsUnion %>%
+  #   mutate(Genero = ifelse(nchar(Genero) == 10, "male", Genero)) 
+  
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Genero = ifelse(Genero == "NULL", "ND", Genero)) 
+  
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Genero = ifelse(Genero == "female", "Mujer", Genero)) 
+  
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Genero = ifelse(Genero == "male", "Hombre", Genero))
+  
+  # tweetsUnion<- tweetsUnion %>%
+  #   mutate(Ciudad = ifelse(Ciudad == "" || nchar(Ciudad) == 0, "ND", tweetsUnion$Ciudad)) 
+  # 
+  # tweetsUnion<- tweetsUnion %>%
+  #   mutate(Ciudad = sapply(tweetsUnion$Ciudad, fillCitiesBlank)) 
+  # 
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Ciudad = ifelse(nchar(Ciudad) == 0, "ND", tweetsUnion$Ciudad)) 
+  
+ #verifica valores na y reemplaza   
+  tweetsUnion[is.na(tweetsUnion)] <- 0
+  
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Ciudad = ifelse(Ciudad == 0, "ND", tweetsUnion$Ciudad)) 
+  
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Pais = ifelse(Pais == 0, "ND", tweetsUnion$Pais)) 
+  
+  
+  tweetsUnion<- tweetsUnion %>%
+    mutate(Ciudad =  sapply(tweetsUnion$Ciudad, createAndSearchCity)) 
+  
+ 
+
+  return(tweetsUnion)
+  
+}
+
+
+#crear corpus
+createCorpus <- function(text){
+  library(tm)
+  #corpus <- iconv(text, to = 'UTF-8-mac') #presnta error cuando se despliega en la nube
+  corpus <- iconv(text, to = 'UTF-8') #corrige despliege en la nube
+  corpus <- Corpus(VectorSource(corpus))
+}
+
+#proceso #3: limpieza datos
+
+cleanDataTweets <- function(tweetText){
+  opinionText <- tweetText
+  opinionText$text <- gsub("(RT|via)|((?:\\b\\w*@\\w+)+)", "", opinionText$text)
+  opinionText$text <- gsub("\\n", "", opinionText$text )
+  opinionText$text <- gsub("@\\w+", "", opinionText$text )
+  opinionText$text <- gsub("[[:punct:]]", "", opinionText$text)
+  opinionText$text <- gsub("[[:digit:]]", "", opinionText$text)
+  opinionText$text <- gsub("http\\w+", "", opinionText$text)
+  opinionText$text <- gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", opinionText$text)
+  #opinionText$text.x <- iconv(opinionText$text.x, to="ASCII//TRANSLIT")
+  opinionText$text  <- iconv(opinionText$text, "UTF-8", "latin1", sub="") #mantiene las ñ y borrar emoticons
+  #opinionText$text.x  <- iconv(opinionText$text.x , "latin1", "ASCII", sub="")  #elimina las ñ, tildes
+  opinionText$text <- chartr('áéíóúñ','aeioun', opinionText$text)
+  
+  return(opinionText$text)
+}
+
+#version 2 para limpieza de texto de tweet
+#devulte un vector
+cleanDataTweetsV2 <- function(corpus, searchText){
+  
+  #convertir a minuscula
+  corpus <- tm_map(corpus, tolower)
+  #quitar la frase o texto de busqueda para evitar redundancia en el texto
+  corpus <- tm_map(corpus, removeWords, c(searchText)) #este debe ir antes de quitar las puntuaciones
+  
+  #quitar caracteres de puntuacion
+  corpus <- tm_map(corpus, removePunctuation)
+  #quitar numeros
+  corpus <- tm_map(corpus, removeNumbers)
+  
+  #remover palabras sin mucho significado y relevancia para el análisis 
+  cleanCorpus <- tm_map(corpus, removeWords, stopwords("es"))
+  #remover carcteres de direccion web
+  cleanCorpus <- tm_map(cleanCorpus, content_transformer(removeURL))
+   #remover los doble espacion
+  cleanCorpus <- tm_map(cleanCorpus, stripWhitespace)
+  
+  #funcion para remover emoticos y mantener las palabras español
+  cleanCorpus <- tm_map(cleanCorpus, content_transformer(removeEmoticons))
+  
+}
+
+#remover direcciones web
+removeURL <- function(x) gsub("http[[:alnum:]]*", '', x)
+
+#remover emoticones
+removeEmoticons  <- function(x) iconv(x, "UTF-8", "latin1", sub="")
+
+#estructurar los datos del tweet en una matriz, recibe un vector corpus limpio
+structureDataTweet <- function(cleanCorpus){
+  tdm <- TermDocumentMatrix(cleanCorpus)
+  tdm <- as.matrix(tdm)
+}
+
+#debido a que la grafica de barra puede volverse muy lento al tratar de graficar cada palabra, vamos a crear subconjuntos 
+#de palabras 
+sumWordFrecuency <- function(tdm){
+  w <- rowSums(tdm)
+  
+  #w <- subset(w, w>=20)
+  
+  subSetWord <- subset(w, w>=20)
+  
+  
+  if(length(subSetWord) == 0){
+    subSetWord <- subset(w, w>=2)
+    
+  }
+ 
+  return (subSetWord)
+}
+
+#retorna tweets con el formato establecido y limpio
+dataTweetsFormat <- function(tweets){
+  
+  cleanDataTweetsFormat <- tweets %>%
+    mutate(TextDepurado = cleanDataTweets(tweetsUnion))
+  
+  return(cleanDataTweetsFormat)
+}
+
+#nube de palabras
+
+calcWordcloud <- function(tdm){
+  library(wordcloud)
+  freqWord <- sort(rowSums(tdm), decreasing = TRUE)
+  set.seed(375)
+  wordcloud(words = names(freqWord),
+            freq = freqWord,
+            random.order = FALSE,
+            max.words = 200,
+            min.freq = 5,
+            colors = brewer.pal(8, "Dark2"),
+            scale = c(5, 0.4),
+            rot.per = 0.5)
+}
+
+# cargar datos locales
+loadLocalData <- function(fileName){
+  
+  if(file.exists(fileName)){
+    dataLocal <- tryCatch(read_csv(fileName), error=function(e) 1)
+  }
+  
+  return(dataLocal)
+
+}
+
+getGender <- function(names){
+  
+ # localGender <- gender::gender(names) %>%
+   # select( c(gender))
+  
+ #gender <- gsub("[[:space:]]", "", localGender$gender)
+  #return (gender)
+  #return(localGender$gender)
+  
+  
+  genderResult <- unique(findGivenNames(names))
+  
+  return(genderResult$gender)
+  
+}
+
+calcSentiment <- function(tweetsData, searchedTweet){
+  
+  
+  tweets <- cleanDataTweets(tweetsData)
+  #tweets <- cleanDataTweetsV2(tweetsData$text, searchedTweet) funciona con tm
+  
+  
+  
+  keyCustomEC <- data.frame(words = tolower(lexico_ec$word),
+                            polarity = lexico_ec$value,
+                            stringsAsFactors = FALSE)
+
+  myKeyCustomEC <- as_key(keyCustomEC)
+
+  if(is_key(myKeyCustomEC) == TRUE){
+ 
+    result <- sentiment(tweets, polarity_dt = myKeyCustomEC)
+
+  }
+  
+ 
+    
+
+  return(result)
+}
+
+#funcion para el calculo de la polaridad
+calcPolarity <- function(sentimentResult){
+  
+    #calcular el valor promedio ya que necesitamos el calculo a nivel de tweet y no a nivel de sentencia
+    sentiment.score <- sentimentResult %>%
+      group_by(element_id) %>%
+      summarise(sentiment = mean(sentiment))
+    
+    sentiment.score.df <- as.data.frame(sentiment.score)
+    
+   
+    colnames(sentiment.score.df)
+    names(sentiment.score.df)[1] <- "ID"
+    names(sentiment.score.df)[2] <- "polarity"
+
+    # #descartar valores neutros
+    sentiment.score.df <- sentiment.score.df[sentiment.score.df$polarity != 0, ]
+    sentiment.score.df <- sentiment.score.df %>%
+      mutate(sentiment = ifelse(sentiment.score.df$polarity <0 , "Negativo", "Positivo"))
+
+    sentiment.score.df$sentiment <- as.factor(sentiment.score.df$sentiment)
+
+    #convertir tabla a data frame
+    sentiment.score.table <- table(sentiment.score.df$sentiment)
+    sentiment.score.df <- data.frame(sentiment.score.table)
+    colnames(sentiment.score.df)
+    names(sentiment.score.df)[1] <- "Sentimiento"
+    names(sentiment.score.df)[2] <- "Polaridad"
+
+  
+  
+  return(sentiment.score.df)
+}
+
+#busqueda datos twitter
+searchTweets <- function(searchText, sinceDate, untilDate, geoCode, ratio){
+  
+  valueOp <- ifelse(is.na(sinceDate), 0, 1)
+  sinceDate <- switch(valueOp+1, 
+                      NULL,
+                      as.character(sinceDate))
+  valueOp <- ifelse(is.na(untilDate), 0, 1)
+  
+  untilDate <- switch(valueOp+1, 
+                      NULL,
+                      as.character(untilDate))
+  
+  geoCodeValue <-paste(geoCode$lat[1],",",geoCode$lng[1], ",",ratio,"km",sep = '')
+  
+  #tweets <- searchTwitter(searchText, lang='es',since= sinceDate, until= untilDate,geocode = '-0.22985,-78.5249481,30km') #Quito, tiene datos OK
+  tweets <- searchTwitter(searchText, lang='es',since= sinceDate, until= untilDate,geocode = geoCodeValue) #Quito, tiene datos OK
+  
+  #Convirtiendo los tweets en un data frame
+  tweetsDF.df <- twListToDF(tweets)%>%
+    mutate(rowNumber = row_number())
+  
+}
+
+#agrega nueva columna para polaridad
+tweetsDataPolarity <- function(tweetsFormat, tweetsSentiment){
+  
+  tweetsFormat %>%
+    mutate(Polarity =  tweetsSentiment) 
+}
+
+#crear ciudad
+createAndSearchCity <- function(citySet){
+  
+  for(val in citySet){
+    city_token <- syuzhet::get_tokens(val)
+    
+    citySelect <- searchCity(city_token)
+    
+  }
+  
+  return (citySelect)
+}
+
+#busca ciudades 
+searchCity <- function(cities){
+  
+  city <- "ND"
+  for (val in cities) { 
+ 
+    value <- toupper(val) %in% toupper(cities_ec$nombre)
+   
+    if(value == TRUE){
+      city <- val
+      break
+    }
+  }
+  
+    
+  return (city)
+}
+
+
+
---
+++ ui.R
@@ -0,0 +1,191 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(leaflet.extras)
+library(shinythemes)
+#library(ggthemr) 
+
+
+header <- dashboardHeader(dropdownMenuOutput("task_menu" ), title = "Products & Services Trends", titleWidth =  280) 
+                          
+sideBar <- dashboardSidebar( width = 280,
+  
+  sidebarMenu( 
+    
+    menuItem("Datos locales", tabName = "analisys"),
+    menuItem("Geolocalización", tabName = "geolocalizacion")
+  )
+  
+  
+  
+)
+
+body <- dashboardBody( 
+  
+  tabItems( 
+    tabItem(tabName = "analisys",
+            titlePanel("Carga de archivo", windowTitle = "Análisis sentimiento Twitter"),
+            tabsetPanel(type = "tabs", id= "tabsetPanelLocalData",
+                        tabPanel(title ="Análisis sentimiento", value = 1,
+                                 wellPanel(
+                                   fluidRow(
+                                     column( width = 12,
+                                             box( width = NULL,collapsible = TRUE,
+                                                  
+                                                  fileInput("fileLoaded", label = "Archivo",
+                                                            buttonLabel = "Cargar archivo",
+                                                            accept =c("csv",
+                                                                      "text/comma-separated-values",
+                                                                      ".csv"))
+                                                  #radioButtons("showData", "Mostrar datos", choices = c("Si"=1, "No"=2), selected = 2)
+                                             )
+                                     ))
+                                   
+                                   
+                                 ),
+                                 wellPanel(
+                                   fluidRow(
+                                     column( width = 12,
+                                             box( width = NULL,collapsible = TRUE, 
+                                                  
+                                                  
+                                                  #selectInput("citySelected", "Ciudades", choices = c("Quito", "Guayaquil", "Cuenca")),
+                                                  htmlOutput("CitiesLoaded"),
+                                                  #radioButtons("genero", "Genero", choices = c("Femenino", "Masculino")), 
+                                                  dateRangeInput("fromToDate", language = 'es', label = "Rango fecha", separator = "hasta", weekstart = 1),
+                                                  p(),
+                                                  actionButton("calcSentiment", label = "Análisis")
+                                                  
+                                             )
+                                     ))
+                                   
+                                   
+                                 ),
+                                 titlePanel("Análisis general"),
+                                 tabsetPanel(type = "tabs", id= "tabsetLocalResult", 
+                                             tabPanel(title = "Trazado a nivel de oración", 
+                                             wellPanel(
+                                               fluidRow(
+                                                 
+                                                 tableOutput(outputId = "dataTweets"),
+                                                 plotOutput(outputId = "scatterplot")
+                                               ))),
+                                             tabPanel(title = "Diagrama de barras", 
+                                                      wellPanel(
+                                                        fluidRow(
+                                                          plotOutput(outputId = "barPlotPolarity")
+                                                        )
+                                                      )
+                                                      
+                                             ), 
+                                             tabPanel(title = "Matriz de término de documentos", 
+                                                      wellPanel(
+                                                        fluidRow(
+                                                          plotOutput(outputId = "barPlotTDM")
+                                                        ))
+                                                      
+                                             ), 
+                                             tabPanel(title = "Nube de palabras", 
+                                                      wellPanel(
+                                                        fluidRow(
+                                                          plotOutput(outputId = "wordCloudPlot")
+                                                        ))
+                                                      
+                                             )
+                                    )               
+                            ),
+                           # conditionalPanel( condition="input.showData == 1",
+                            #                 wellPanel(
+                           tabPanel( title="Datos locales", value = 2,
+                                     br(),
+                                     fluidRow(DT::dataTableOutput(outputId = "twetterDataLocal"))
+                                     
+                           )
+                                             #)
+                             
+                            
+            
+            )
+    ),
+    tabItem(tabName = "geolocalizacion",
+            titlePanel("Geolocalización Twitter", windowTitle = "Análisis sentimiento Twitter por Geolocalización"),
+            tabsetPanel(type = "tabs", id= "tabsetPanel",
+                        tabPanel(title ="Geolocalización", 
+                                 
+                                 wellPanel(
+                                   
+                                   fluidRow(
+                                     column( width = 12,
+                                             box( width = NULL,collapsible = TRUE,
+                                                  
+                                                  textInput("geoSearch", label = "Texto de búsqueda", placeholder = "Ingrese producto o servicio"),
+                                                  
+                                                  dateRangeInput("geoFromToDate", language = 'es', label = "Rango fecha", separator = "hasta", weekstart = 1),
+                                                  numericInput("geoRatio", "Cobertura(km)", min = 0, max = 500,value = 10, width = 200),
+                                                  
+                                                  actionButton("calcGeoSentiment", label = "Análisis"),
+                                                  p(),
+                                                  leafletOutput("EcuadorMap")
+                                                  
+                                             )
+                                     )
+                                     
+                                   )
+                                   
+                                 ),
+                                
+                                 titlePanel("Análisis general", windowTitle = "Resultado análisis sentimiento"),
+                                 tabsetPanel(type = "tabs", id= "tabsetResult", 
+                                             tabPanel(title = "General", 
+                                                      
+                                                      wellPanel(plotOutput(outputId = "barplot"))
+                                                      
+                                             ), 
+                                             tabPanel(title = "Día", 
+                                                      wellPanel(plotOutput(outputId = "dayPlot"))
+                                                      
+                                             ), 
+                                             tabPanel(title = "Género", 
+                                                      wellPanel(plotOutput(outputId = "genPolarityPlot"))
+                                                      
+                                             ), 
+                                             tabPanel(title = "Ciudad", 
+                                                      wellPanel(plotOutput(outputId = "cityPolarityPlot"))
+                                                      
+                                             )
+                                             
+                                 )
+                                 
+                                
+                                 
+                        ),
+                        
+                        tabPanel( title="Datos",
+                                  br(),
+                                  DT::dataTableOutput(outputId = "twitterData")
+                                  
+                        )
+                        
+            )
+            
+            
+            
+            
+    )
+    
+  )
+)
+
+#permite enviar las secciones al estilo dashboard
+ui <- dashboardPage(header, sideBar, body, skin = "purple")
+
+
