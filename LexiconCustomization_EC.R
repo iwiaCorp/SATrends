900fa02637000e57e358eb8689f1395074a3088b
Ismael Utitiaj <eutitiaj@gmail.com>
2019-10-24 10:11:57 -0500

    primer envio de componentes herramiento al repositorio

---
+++ LexiconCustomization_EC.R
@@ -0,0 +1,17 @@
+library(readr)
+
+fileNameLexico_EC <- "lexico_ec_custom.csv"
+
+if(file.exists(fileNameLexico_EC)){
+  lexico_ec <- tryCatch(read_csv(fileNameLexico_EC), error=function(e) 1)
+  
+}
+  
+fileNameCities_EC <- "tabla_ciudad_EC.csv"
+
+if(file.exists(fileNameLexico_EC)){
+  cities_ec <- tryCatch(read_csv(fileNameCities_EC), error=function(e) 1)
+  
+}
+
+
